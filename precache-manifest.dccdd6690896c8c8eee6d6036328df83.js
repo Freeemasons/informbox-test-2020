41ffe06955a72e409b0c69d352e77e70a5f678eb
freemasons <shivasmile11@gmail.com>
2020-09-09 04:49:40 +0300

    Updates

---
+++ precache-manifest.dccdd6690896c8c8eee6d6036328df83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a624f008e020ecab32eb6e313a77cb5f",
+    "url": "/informbox-test-2020/index.html"
+  },
+  {
+    "revision": "ef38bf316202a66bff5a",
+    "url": "/informbox-test-2020/static/css/2.48707a75.chunk.css"
+  },
+  {
+    "revision": "da5ab53ad70fb418d589",
+    "url": "/informbox-test-2020/static/css/main.07e7ce89.chunk.css"
+  },
+  {
+    "revision": "ef38bf316202a66bff5a",
+    "url": "/informbox-test-2020/static/js/2.a88c2762.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/informbox-test-2020/static/js/2.a88c2762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5ab53ad70fb418d589",
+    "url": "/informbox-test-2020/static/js/main.49f307fe.chunk.js"
+  },
+  {
+    "revision": "c2c95b5617fc73930637",
+    "url": "/informbox-test-2020/static/js/runtime-main.3fcc79ac.js"
+  }
+]);
