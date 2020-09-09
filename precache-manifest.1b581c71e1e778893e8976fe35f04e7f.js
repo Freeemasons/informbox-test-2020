3dae32286a87c4c3d18f0757fa17cdfb6bf7dcf7
freemasons <shivasmile11@gmail.com>
2020-09-09 05:46:52 +0300

    Updates

--- precache-manifest.1b581c71e1e778893e8976fe35f04e7f.js
+++ precache-manifest.1b581c71e1e778893e8976fe35f04e7f.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0756561cd43b247cfc815b0ed7ed23b8",
+    "revision": "627a35b4fdf4c66cfb355dd300275fff",
     "url": "/informbox-test-2020/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/informbox-test-2020/static/css/2.48707a75.chunk.css"
   },
   {
-    "revision": "cf2e4dd194a2ad944dca",
-    "url": "/informbox-test-2020/static/css/main.96726ef8.chunk.css"
+    "revision": "2dd17c12ff78f92d2a91",
+    "url": "/informbox-test-2020/static/css/main.6bdeca11.chunk.css"
   },
   {
     "revision": "ef38bf316202a66bff5a",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/informbox-test-2020/static/js/2.a88c2762.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cf2e4dd194a2ad944dca",
-    "url": "/informbox-test-2020/static/js/main.46e6f6f3.chunk.js"
+    "revision": "2dd17c12ff78f92d2a91",
+    "url": "/informbox-test-2020/static/js/main.bca42825.chunk.js"
   },
   {
     "revision": "c2c95b5617fc73930637",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/informbox-test-2020/precache-manifest.fd788b56490082bf50e23c7d67063ed6.js"
+  "/informbox-test-2020/precache-manifest.1b581c71e1e778893e8976fe35f04e7f.js"
 );
 
 self.addEventListener('message', (event) => {
